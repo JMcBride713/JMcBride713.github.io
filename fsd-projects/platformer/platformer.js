28a634196f172804834fde023367b18b1fbb872c
perkingsnkabyo <perkingsnkabyo2gmail.com>
2025-11-07 13:50:33 -0600

    Jacob's website Nov. 7

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -27,24 +27,34 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+     toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
+createPlatform(100, 700, 10, 10, "lime");
+createPlatform(900, 700, 10, 10, "Blue")
+createPlatform(650, 370, 10, 10, "Red")
+createPlatform(600, 470, 10, 10, "Violet")
+createPlatform(550, 600, 10, 10, "Green")
+createPlatform(500, 450, 10, 10, "Cyan")
 
     // TODO 3 - Create Collectables
+createCollectable("steve", 1000, 450);
+createCollectable("diamond", 250, 200, 0.5, 0.7);
+createCollectable("grace", 135, 170,);
+createCollectable("kennedi", 300, 250,);
+createCollectable("max", 350, 300,);
+createCollectable("steve", 400, 350,);
+createCollectable("database", 450, 400,);
+createCollectable("database", 500, 450,);
+createCollectable("HEART_LOCKET", 550, 500,);
+createCollectable("REAL_KNIFE", 600, 550,);
 
-
-
-    
     // TODO 4 - Create Cannons
+createCannon("top", 200, 500);
+createCannon("bottom", 800, 400);
+createCannon("top", 1070, 700)
 
-
-    
-    
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
     //////////////////////////////////
--- fsd-projects/platformer/setup.js
+++ fsd-projects/platformer/setup.js
@@ -1,7 +1,7 @@
 // setup variables
 const walkAcceleration = 2.5; // how much is added to the speed each frame
 const gravity = 0.5; // how much is subtracted from speedY each frame
-const friction = 1.5; // how much the player is slowed each frame
+const friction = 1.5; // how much the player is slowed each frame -- original
 const maxSpeed = 8; // maximum horizontal speed, not vertical
 const playerJumpStrength = 12; // this is subtracted from the speedY each jump
 const projectileSpeed = 8; // the speed of projectiles
@@ -14,7 +14,7 @@ let gridMade = false;
 
 // Base game variables
 const frameRate = 60;
-const playerScale = 0.8; //makes the player just a bit smaller. Doesn't affect the hitbox, just the image
+const playerScale = 0.7; //makes the player just a bit smaller. Doesn't affect the hitbox, just the image
 
 // Player variables
 const player = {
@@ -100,4 +100,6 @@ var collectableList = {
   kennedi: { image: "images/collectables/kennedi-head.png" },
   max: { image: "images/collectables/max-head.png" },
   steve: { image: "images/collectables/steve-head.png" },
+  REAL_KNIFE: { image: "images/collectables/REAL_KNIFE.png" },
+  HEART_LOCKET: {image: "images/collectables/HEART_LOCKET.png"},
 };
