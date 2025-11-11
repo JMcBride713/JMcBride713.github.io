8f74d48bf49773807fa68d9e3de33cf7638c89dc
perkingsnkabyo <perkingsnkabyo2gmail.com>
2025-11-11 13:25:55 -0600

    text describe update

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -33,27 +33,32 @@ $(function () {
     // TODO 2 - Create Platforms
 createPlatform(100, 700, 10, 10, "lime");
 createPlatform(900, 700, 10, 10, "Blue")
-createPlatform(650, 370, 10, 10, "Red")
+createPlatform(650, 340, 10, 10, "Red")
 createPlatform(600, 470, 10, 10, "Violet")
 createPlatform(550, 600, 10, 10, "Green")
-createPlatform(500, 450, 10, 10, "Cyan")
+createPlatform(400, 350, 10, 10, "Cyan")
 
     // TODO 3 - Create Collectables
 createCollectable("steve", 1000, 450);
 createCollectable("diamond", 250, 200, 0.5, 0.7);
 createCollectable("grace", 135, 170,);
 createCollectable("kennedi", 300, 250,);
-createCollectable("max", 350, 300,);
+createCollectable("max", 900, 200,);
 createCollectable("steve", 400, 350,);
 createCollectable("database", 450, 400,);
-createCollectable("database", 500, 450,);
-createCollectable("HEART_LOCKET", 550, 500,);
-createCollectable("REAL_KNIFE", 600, 550,);
+createCollectable("database", 650, 150,);
+createCollectable("HEART_LOCKET", 750, 450,);
+createCollectable("REAL_KNIFE", 500, 200,);
 
     // TODO 4 - Create Cannons
 createCannon("top", 200, 500);
-createCannon("bottom", 800, 400);
-createCannon("top", 1070, 700)
+createCannon("bottom", 400, 450);
+createCannon("top", 1070, 700);
+createCannon("top", 1070, 701);
+createCannon("bottom", 400, 451)
+createCannon("top", 200, 501)
+createCannon("right", 300, 600)
+createCannon("right", 500, 550)
 
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
